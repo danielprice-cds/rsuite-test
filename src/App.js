11a649f43e3f6e052a30e50227329ac264a167fe
Daniel Price <dbp@cds-systems.co.uk>
2023-02-23 15:28:44 +0000

    rsuite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { InputPicker } from 'rsuite';
+import SpinnerIcon from '@rsuite/icons/legacy/Spinner';
+import 'rsuite/dist/rsuite.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const useUsers = (defaultUsers = []) => {
+    const [users, setUsers] = useState(defaultUsers);
+    const [loading, setLoading] = useState(false);
+    const featUsers = word => {
+        setUsers([
+            {
+                "id": "tim",
+                "login": "Tim"
+            }
+        ]);
+
+        setLoading(false);
+    };
+
+    return [users, loading, featUsers];
+};
+
+const App = () => {
+    const [users, loading, featUsers] = useUsers();
+    return (
+        <InputPicker
+            data={users}
+            style={{ width: 224 }}
+            labelKey="login"
+            valueKey="id"
+            id='appUserPicker'
+            data-testid="app.userPicker"
+            onSearch={featUsers}
+            renderMenu={menu => {
+                if (loading) {
+                    return (
+                        <p style={{ padding: 10, color: '#999', textAlign: 'center' }}>
+                            <SpinnerIcon spin /> Loading...
+                        </p>
+                    );
+                }
+                return menu;
+            }}
+        />
+    );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+    render(<App />);
+
+    const picker = await screen.findByTestId('app.userPicker');
+    // eslint-disable-next-line testing-library/no-node-access
+    const input = picker.querySelector('input');
+
+    fireEvent.input(input, {
+        target: { value: 'kero', name: 'appUserPicker' },
+    });
+
+    // eslint-disable-next-line testing-library/no-node-access
+    await waitFor(() => expect(document.body.querySelector('#appUserPicker-listbox > div')).not.toBeNull());
+
+    // eslint-disable-next-line testing-library/no-node-access
+    const option = document.body.querySelector('#appUserPicker-listbox > div')
+
+    console.log(option);
+
+    fireEvent.click(option);
+
+    expect(option).toBeDefined();
 });
